e9a74158f18a92bbad3646a2d06600388bec8078
Dmitry <dm.rubashko@gmail.com>
2021-11-21 15:50:08 +0300

    some fixes

--- app/src/pages/personalDataPage/containers/personalDataPageContainer.js
+++ app/src/pages/personalDataPage/containers/personalDataPageContainer.js
@@ -1,19 +1,9 @@
 import PersonalDataPageLayout from "../components/personalDataPageLayout";
 
-import {useEffect} from 'react';
 import {useSelector, useDispatch} from "react-redux";
 
-import {GET_ORDER_REQUEST} from '../actions';
-
 const PersonalDataPageContainer = () => {
-  // const dispatch = useDispatch();
   const {info} = useSelector((state) => state.auth)
-  // const {ordersList, totalPrice, isLoading} = useSelector((state) => state.personalDataPageReducer)
-  // const {itemsList} = useSelector((state) => state.cart)
-
-  // useEffect(() => {
-  //   dispatch(GET_ORDER_REQUEST())
-  // }, [dispatch])
 
   return <PersonalDataPageLayout
     firstName={info.firstName}
@@ -21,11 +11,6 @@ const PersonalDataPageContainer = () => {
     gender={info.gender}
     email={info.email}
     phone={info.phone}
-
-    // itemsList={itemsList}
-    // ordersList={ordersList}
-    // totalPrice={totalPrice}
-    // isLoading={isLoading}
   />
 };
 
